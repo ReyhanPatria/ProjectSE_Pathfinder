b9d1a6ed7c35a5411a08fb1ef98507904f34dbde
ReyhanPatria <reyhan2patria@gmail.com>
2021-01-23 02:22:47 +0700

    Revised A-Star algorithm

--- src/algorithm/aStar.js
+++ src/algorithm/aStar.js
@@ -1,18 +1,25 @@
 export function aStar(board) {
-    console.log("a star running");
-
-    // Initialize current node list
+    // Initialize start node g value as 0
     board.startNode.g = 0;
+    
+    // Initialize current node list
     let nodeList = [board.startNode];
     let nodeHistory = [];
+
+    // Algorithm loop
     while(nodeList.length > 0) {
         // Select node with lowest F value
         let currentNode = nodeList[0];
         for(let i = 0; i < nodeList.length; i++) {
             let currentNodeF = currentNode.g + currentNode.getDistance(board.endNode);
             let nextNodeF = nodeList[i].g + nodeList[i].getDistance(board.endNode);
-            if(nextNodeF <= currentNodeF) {
+            if(nextNodeF < currentNodeF) {
                 currentNode = nodeList[i];
+            }
+            else if(nextNodeF == currentNodeF) {
+                if(nodeList[i].getDistance(board.endNode) < currentNode.getDistance(board.endNode)) {
+                    currentNode = nodeList[i];
+                }
             }
         }
 
@@ -22,8 +29,7 @@ export function aStar(board) {
         nodeHistory.push(currentNode);
 
         // Update currentNode status to visited
-        if(currentNode.status != "startNode" &&
-            currentNode.status != "endNode") {
+        if(currentNode.status != "startNode" && currentNode.status != "endNode") {
             currentNode.status = "visited";
         }
 
@@ -33,28 +39,43 @@ export function aStar(board) {
             return nodeHistory;
         }
 
+        // Define movement
         let moveX = [+0, +1, +0, -1];
         let moveY = [-1, -0, +1, -0];
         let moves = moveX.length;
+        // Loop through currentNode neighbours
         for(let i = 0; i < moves; i++) {
+            // Next node x and y
             let nextX = currentNode.x + moveX[i];
             let nextY = currentNode.y + moveY[i];
+
+            // Check if next node is out of bounds
             if(nextX < 0 || nextX >= board.width || 
                 nextY < 0 || nextY >= board.height) {
                 continue;
             }
 
+            // Get nextNode object
             let nextNode = board.array[nextY][nextX];
+            
+            // Check if nextNode is a wall
             if(nextNode.status == "wall") {
                 continue;
             }
 
+            // Define nextNode temporary g value
             let nextNodeTempG = 1 + currentNode.g;
+            // Check if nextNode's temporary g value is lower than actual g value
             if(nextNodeTempG < nextNode.g) {
-                nextNode.addParent(currentNode);
+                // Update nextNode's parent to currentNode
+                nextNode.setParent(currentNode);
+                // Update nextNode's g value
                 nextNode.g = nextNodeTempG;
 
-                nodeList.push(nextNode);
+                // If nextNode is not in nodeList, then add it
+                if(nodeList.includes(nextNode) == false) {
+                    nodeList.push(nextNode);
+                }
             }
         }
     }
